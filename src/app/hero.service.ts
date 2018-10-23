3db600478992a38ae3236b77ac605f5bdfad9878
Valdionor Junior <valdionor.junior@beltsysplus.com.br>
2018-10-23 16:13:43 -0300

    HTTP
    Modulo de estudos com HTTP

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,26 +1,31 @@
 import { Injectable } from '@angular/core';
-
 import { Observable, of } from 'rxjs';
-
 import { Hero } from './hero';
-import { HEROES } from './mock-heroes';
 import { MessageService } from './message.service';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import { HEROES } from './mock-heroes';
 
 @Injectable({
   providedIn: 'root',
 })
 export class HeroService {
+  
+  private heroesUrl= 'api/heroes';// URL to web api
 
-  constructor(private messageService: MessageService) { }
+  constructor(private http: HttpClient, private messageService: MessageService) { }
 
   getHeroes(): Observable<Hero[]> {
-    // TODO: send the message _after_ fetching the heroes
-    this.messageService.add('HeroService: fetched heroes');
-    return of(HEROES);
+   
+    return  this.http.get<Hero[]>(this.heroesUrl)
   }
+  
   getHero(id: number): Observable<Hero> {
     // TODO: send the message _after_ fetching the hero
     this.messageService.add(`HeroService: fetched hero id=${id}`);
     return of(HEROES.find(hero => hero.id === id));
+  }
+
+  private log(message: string){
+    this.messageService.add('HeroService: ${message}')
   }
 }
